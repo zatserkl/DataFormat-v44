2993e3305c108782f8d628903db020463422d76e
Andriy Zatserklyaniy <zatserkl@gmail.com>
2014-05-05 23:32:09 -0700

    Commented out loading of the ~/macros/utils.C (my private ROOT library).

--- rootlogon.C
+++ rootlogon.C
@@ -1,6 +1,6 @@
 {
-gROOT->Macro(gSystem->ExpandPathName("$(HOME)/macros/rootlogon.C"));
-cout<< "*-- Local rootlogon" << endl;
+// gROOT->Macro(gSystem->ExpandPathName("$(HOME)/macros/rootlogon.C"));
+// cout<< "*-- Local rootlogon" << endl;
 
 cout<< "Load DataFormat.C+" <<endl;
 gROOT->LoadMacro("DataFormat.C+");
