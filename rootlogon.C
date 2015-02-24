955f788790987d7978cec5a7d1f4232f59be6efd
Andriy Zatserklyaniy <zatserkl@gmail.com>
2015-02-24 12:02:47 -0800

    replaced processRun.C by recoRun.C

--- rootlogon.C
+++ rootlogon.C
@@ -1,10 +1,16 @@
 {
 // gROOT->Macro(gSystem->ExpandPathName("$(HOME)/macros/rootlogon.C"));
-// cout<< "*-- Local rootlogon" << endl;
+cout<< "*-- Local rootlogon" << endl;
 
 cout<< "Load DataFormat.C+" <<endl;
 gROOT->LoadMacro("DataFormat.C+");
 
-cout<< "Load processRun.C+" <<endl;
-gROOT->LoadMacro("processRun.C+");
+//cout<< "Load processRun.C+" <<endl;
+//gROOT->LoadMacro("processRun.C+");
+
+cout<< "Load recoRun.C+" <<endl;
+gROOT->LoadMacro("recoRun.C+");
+
+//cout<< "Load online.C+" <<endl;
+//gROOT->LoadMacro("online.C+");
 }
